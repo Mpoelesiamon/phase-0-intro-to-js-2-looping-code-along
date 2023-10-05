c1575c6cca8b017e2f59c89181b056851be53e3b
lesiamon <lesiamon.mpoe@student.moringaschool.com>
2023-10-05 22:45:19 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+//writeCards Function
+function writeCards(names, eventName) {
+    const thankYouCards = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouCards.push(thankYouMessage);
+    }
+  
+    return thankYouCards;
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  
+  const thankYouCards = writeCards(names, eventName);
+  console.log(thankYouCards);
+
+  //countDown Function
+  function countDown(positiveInteger) {
+    let count = positiveInteger;
+  
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  countDown(10);
